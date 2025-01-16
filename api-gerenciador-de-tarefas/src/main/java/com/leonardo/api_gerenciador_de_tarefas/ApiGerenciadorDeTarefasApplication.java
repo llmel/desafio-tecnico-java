f0a6b70f55fe0f1919bb2cb8c6824a81a8e63662
lcmelo2 <lcmelo2@stefanini.com>
2025-01-16 18:34:11 -0300

    Criado boilerplate da aplicação, configurado conexão com o banco de dados e configurado a porta local para execução da aplicação

---
+++ api-gerenciador-de-tarefas/src/main/java/com/leonardo/api_gerenciador_de_tarefas/ApiGerenciadorDeTarefasApplication.java
@@ -0,0 +1,13 @@
+package com.leonardo.api_gerenciador_de_tarefas;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class ApiGerenciadorDeTarefasApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(ApiGerenciadorDeTarefasApplication.class, args);
+	}
+
+}
---
+++ api-gerenciador-de-tarefas/src/test/java/com/leonardo/api_gerenciador_de_tarefas/ApiGerenciadorDeTarefasApplicationTests.java
@@ -0,0 +1,13 @@
+package com.leonardo.api_gerenciador_de_tarefas;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class ApiGerenciadorDeTarefasApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
