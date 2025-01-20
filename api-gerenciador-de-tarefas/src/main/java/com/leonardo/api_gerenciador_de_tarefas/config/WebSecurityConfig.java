428754a253df2bd4741e046d0d821096af319344
llmel <contatoleonardomelo@hotmail.com>
2025-01-20 17:29:56 -0300

    Adicionei o pacote springdoc ao projeto, que nada mais é do que o swagger para documentação da API. Precisei mexer nas configurações do spring security também, porque quando eu desabilitei o csrf, por consequência da criação do arquivo de configuração, a autenticação básica foi desabilitada, então precisei habilitá-la no arquivo de configuração.

--- api-gerenciador-de-tarefas/src/main/java/com/leonardo/api_gerenciador_de_tarefas/config/WebSecurityConfig.java
+++ api-gerenciador-de-tarefas/src/main/java/com/leonardo/api_gerenciador_de_tarefas/config/WebSecurityConfig.java
@@ -2,6 +2,7 @@ package com.leonardo.api_gerenciador_de_tarefas.config;
 
 import org.springframework.context.annotation.Bean;
 import org.springframework.context.annotation.Configuration;
+import org.springframework.security.config.Customizer;
 import org.springframework.security.config.annotation.web.builders.HttpSecurity;
 import org.springframework.security.config.annotation.web.configuration.EnableWebSecurity;
 import org.springframework.security.web.SecurityFilterChain;
@@ -12,7 +13,12 @@ public class WebSecurityConfig {
     @Bean
     public SecurityFilterChain securityFilterChain(HttpSecurity http) throws Exception {
         http
-                .csrf(csrf -> csrf.disable());
+                .csrf(csrf -> csrf.disable())
+                .authorizeHttpRequests(authorize -> authorize
+                        .anyRequest().authenticated()
+                )
+                .httpBasic(Customizer.withDefaults());
+
 
         return http.build();
     }
